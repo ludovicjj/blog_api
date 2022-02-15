10de91741af5705973d04c2e46073cff3536921a
ludovicjj <jahanlud@gmail.com>
2022-02-15 14:55:21 +0100

    sqlite database and entity User

---
+++ src/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     */
+    private ?string $email = null;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+}
