8348559628d7b94dff9f4cda27173ab19791fe8a
ludovicjj <jahanlud@gmail.com>
2022-02-21 21:25:48 +0100

    Custom filter: MySearchFilter, fix build query

--- src/Filter/MySearchFilter.php
+++ src/Filter/MySearchFilter.php
@@ -5,11 +5,7 @@ namespace App\Filter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractContextAwareFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
 use Doctrine\ORM\QueryBuilder;
-use Doctrine\Persistence\ManagerRegistry;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\PropertyInfo\Type;
-use Symfony\Component\Serializer\NameConverter\NameConverterInterface;
 
 class MySearchFilter extends AbstractContextAwareFilter
 {
@@ -22,34 +18,42 @@ class MySearchFilter extends AbstractContextAwareFilter
         $searchValues = [];
 
         if (str_contains($value, ',')) {
-            $searchValues = explode(',', $value);
+            $searchValues = array_map('trim', explode(',', $value));
         } else {
-            $searchValues[] = $value;
+            $searchValues[] = trim($value);
         }
 
-        foreach ($searchValues as $index => $searchValue) {
-            $this->buildAndWhere(
-                $queryBuilder,
-                $index,
-                $searchValue,
-                $queryNameGenerator->generateParameterName('search')
-            );
-        }
+        $searchValues = $this->removeEmptySearchValue($searchValues);
+         if (!empty($searchValues)) {
+             $this->buildQuery($queryBuilder, $queryNameGenerator, $searchValues);
+         }
     }
 
-    private function buildAndWhere(QueryBuilder $queryBuilder, $index, $value, string $parameterName)
+    /**
+     * @param QueryBuilder $queryBuilder
+     * @param QueryNameGeneratorInterface $queryNameGenerator
+     * @param array $values
+     * @return void
+     */
+    private function buildQuery(QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, array $values): void
     {
         $alias = $queryBuilder->getRootAliases()[0];
-        $orExp = $queryBuilder->expr()->orX(
-            $queryBuilder->expr()->like('LOWER('. $alias. '.title)', ':' . $parameterName)
-        );
-
-        if ($index > 0) {
-            $queryBuilder->orWhere($orExp);
-        } else {
-            $queryBuilder->andWhere($orExp);
+        $orExp = $queryBuilder->expr()->orX();
+        foreach ($values as $value) {
+            $parameterName = $queryNameGenerator->generateParameterName('search');
+            $orExp->add($queryBuilder->expr()->like('LOWER('. $alias. '.title)', ':' . $parameterName));
+            $queryBuilder->setParameter($parameterName, '%' . strtolower($value). '%');
         }
-        $queryBuilder->setParameter($parameterName, '%' . strtolower($value). '%');
+        $queryBuilder->andWhere($orExp);
+    }
+
+    /**
+     * @param array $searchValues
+     * @return array
+     */
+    private function removeEmptySearchValue(array $searchValues): array
+    {
+        return array_filter($searchValues, fn ($value) => !is_null($value) && $value !== '');
     }
 
     public function getDescription(string $resourceClass): array
