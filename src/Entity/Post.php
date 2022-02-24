6fb0d39038ce77957d49a61508f475989404f2c0
ludovicjj <jahanlud@gmail.com>
2022-02-24 14:52:50 +0100

    Declare GET itemOperation return 404

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Action\NotFoundAction;
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
@@ -79,7 +80,10 @@ use DateTime;
     ],
     itemOperations: [
         'get' => [
-            'normalization_context' => ['groups' => ['read:post:collection', 'read:post:item', 'read:user:collection']]
+            'normalization_context' => ['groups' => ['read:post:collection', 'read:post:item', 'read:user:collection']],
+            'controller' => NotFoundAction::class,
+            'read' => false,
+            'output' => false
         ],
         'put' => [
             'denormalization_context' => ['groups' => ['write:post']]
