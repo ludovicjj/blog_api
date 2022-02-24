9ea0a861a417dbeeda7b8c04838a2d48913276db
ludovicjj <jahanlud@gmail.com>
2022-02-24 15:00:27 +0100

    Update responses to GET itemOperation

--- src/OpenApi/OpenApiFactory.php
+++ src/OpenApi/OpenApiFactory.php
@@ -16,6 +16,8 @@ class OpenApiFactory implements OpenApiFactoryInterface
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = $this->decorated->__invoke($context);
+
+        // Update summary to get operation "/api/post/count"
         /** @var PathItem $path */
         foreach ($openApi->getPaths()->getPaths() as $key => $path) {
             if ($path->getGet() && str_contains($path->getGet()->getSummary(), 'Permet')) {
@@ -26,6 +28,19 @@ class OpenApiFactory implements OpenApiFactoryInterface
             }
         }
 
+        // Update responses to get operation "/api/posts/{id}"
+        $path = $openApi->getPaths()->getPath('/api/posts/{id}');
+        $operation = $path->getGet();
+        $openApi->getPaths()->addPath(
+            '/api/posts/{id}',
+            $path->withGet(
+                $operation->withResponses(
+                    array_filter($operation->getResponses(), fn($status) => $status !== 204, ARRAY_FILTER_USE_KEY)
+                )
+            )
+        );
+
+        // add query parameter to get operation "/api/post/count"
         $pathItem = $openApi->getPaths()->getPath('/api/posts/count');
         $operation = $pathItem->getGet();
         $openApi->getPaths()->addPath(
