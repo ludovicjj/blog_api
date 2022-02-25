9055c835115a99d3393246b7eac83689608ec729
ludovicjj <jahanlud@gmail.com>
2022-02-25 18:24:54 +0100

    Add an other dailyStats and define mostPopularListing

--- src/DataProvider/DailyStatsProvider.php
+++ src/DataProvider/DailyStatsProvider.php
@@ -5,16 +5,33 @@ namespace App\DataProvider;
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\DailyStats;
+use App\Repository\PostRepository;
 
 class DailyStatsProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface
 {
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     public function getCollection(string $resourceClass, string $operationName = null)
     {
-        $stats = new DailyStats();
-        $stats->date = new \DateTime();
-        $stats->totalVisitors = 100;
+        $listing = $this->postRepository->findBy([], [], 4);
 
-        return [$stats];
+        $stats1 = new DailyStats(
+            new \DateTime(),
+            100,
+            $listing
+        );
+        $stats2 = new DailyStats(
+            new \DateTime('-1 day'),
+            200,
+            $listing
+        );
+
+        return [$stats1, $stats2];
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
