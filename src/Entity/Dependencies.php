ab6087fd677547c69fd37dc2e56b5ab5a89849aa
ludovicjj <jahanlud@gmail.com>
2022-02-25 13:48:47 +0100

    DependenciesDataProvider

---
+++ src/Entity/Dependencies.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+
+#[ApiResource(
+    collectionOperations: ['get'],
+    itemOperations: ['get'],
+    paginationEnabled: false
+)]
+class Dependencies
+{
+    #[ApiProperty(identifier: true)]
+    private string $uuid;
+
+    #[ApiProperty(
+        description: 'Nom de la dependence',
+        example: "api-platform/core"
+    )]
+    private string $name;
+
+    #[ApiProperty(
+        description: 'Version de la dependence',
+        example: "^2.6"
+    )]
+    private string $version;
+
+    public function __construct($uuid, $name, $version)
+    {
+        $this->uuid = $uuid;
+        $this->name = $name;
+        $this->version = $version;
+    }
+
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getVersion(): string
+    {
+        return $this->version;
+    }
+}
