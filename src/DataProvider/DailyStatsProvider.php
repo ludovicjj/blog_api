d97587c2f01a8581d7637b68326b74d1e26c10fa
ludovicjj <jahanlud@gmail.com>
2022-02-28 08:20:07 +0100

    Update DailyStatsProvider and extends ItemDataProviderInterface

--- src/DataProvider/DailyStatsProvider.php
+++ src/DataProvider/DailyStatsProvider.php
@@ -3,39 +3,31 @@
 namespace App\DataProvider;
 
 use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
+use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\DailyStats;
-use App\Repository\PostRepository;
+use App\Service\StatsHelper;
 
-class DailyStatsProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface
+class DailyStatsProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface,
+    ItemDataProviderInterface
 {
-    private $postRepository;
 
-    public function __construct(PostRepository $postRepository)
+    public function __construct(private StatsHelper $statsHelper)
     {
-        $this->postRepository = $postRepository;
     }
 
-    public function getCollection(string $resourceClass, string $operationName = null)
+    public function getCollection(string $resourceClass, string $operationName = null): array
     {
-        $listing = $this->postRepository->findBy([], [], 4);
-
-        $stats1 = new DailyStats(
-            new \DateTime(),
-            100,
-            $listing
-        );
-        $stats2 = new DailyStats(
-            new \DateTime('-1 day'),
-            200,
-            $listing
-        );
-
-        return [$stats1, $stats2];
+        return $this->statsHelper->fetchMany();
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
         return $resourceClass === DailyStats::class;
+    }
+
+    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?object
+    {
+        return $this->statsHelper->fetchOne($id);
     }
 }
