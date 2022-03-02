9de7fe938176077fadf214eb4d712ffad2f39200
ludovicjj <jahanlud@gmail.com>
2022-03-02 10:02:02 +0100

    req monolog

--- src/DataPersister/DailyStatsPersister.php
+++ src/DataPersister/DailyStatsPersister.php
@@ -2,7 +2,31 @@
 
 namespace App\DataPersister;
 
-class DailyStatsPersister
-{
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use App\Entity\DailyStats;
+use Psr\Log\LoggerInterface;
 
+class DailyStatsPersister implements DataPersisterInterface
+{
+    public function __construct(private LoggerInterface $logger)
+    {
+    }
+
+    public function supports($data): bool
+    {
+        return $data instanceof DailyStats;
+    }
+
+    /**
+     * @param DailyStats $data
+     */
+    public function persist($data)
+    {
+        $this->logger->info(sprintf('Update the  total visitor to "%d"', $data->totalVisitors));
+    }
+
+    public function remove($data)
+    {
+        throw new \Exception('Not supported');
+    }
 }
