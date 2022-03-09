ed8b39d2ac7ad3209ddaa9034e6e5aba84a8a04a
ludovicjj <jahanlud@gmail.com>
2022-03-09 17:23:07 +0100

    Add login path to openapi

--- src/OpenApi/OpenApiFactory.php
+++ src/OpenApi/OpenApiFactory.php
@@ -3,6 +3,9 @@
 namespace App\OpenApi;
 
 use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ApiPlatform\Core\OpenApi\Model\Operation;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\Model\RequestBody;
 use ApiPlatform\Core\OpenApi\OpenApi;
 
 class OpenApiFactory implements OpenApiFactoryInterface
@@ -22,6 +25,67 @@ class OpenApiFactory implements OpenApiFactoryInterface
             'in' => 'cookie',
             'name' => 'JSESSIONID'
         ]);
+
+        $loginPath = new PathItem();
+        $loginOperation = new Operation(
+            operationId: 'apiAuthLogin',
+            tags: ['Security'],
+            responses: [
+                '200' => [
+                    'description' => 'authentication success',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'email' => [
+                                        'type' => 'string'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                '401' => [
+                    'description' => 'authentication failure',
+                    'content' => [
+                        'application/json' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'error' => [
+                                        'type' => 'string'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            summary: 'Authentication',
+            description: 'Authentication',
+            requestBody: new RequestBody(
+                content: new \ArrayObject([
+                    'application/json' => [
+                        'schema' => [
+                            "type" => "object",
+                            'properties' => [
+                                'email' => [
+                                    'type' => 'string',
+                                    'example' => 'cheeselover1@example.com'
+                                ],
+                                'password' => [
+                                    'type' => 'string',
+                                    'example' => 'foo'
+                                ]
+                            ]
+                        ]
+                    ]
+                ])
+            )
+        );
+
+        $openApi->getPaths()->addPath("/login", $loginPath->withPost($loginOperation));
 
         return $openApi;
     }
