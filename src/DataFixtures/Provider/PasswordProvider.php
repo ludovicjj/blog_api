cc93822eb1e508593996b7f9fa88993b6748fdc4
ludovicjj <jahanlud@gmail.com>
2022-03-11 14:48:02 +0100

    Rename arg and add phpdoc

--- src/DataFixtures/Provider/PasswordProvider.php
+++ src/DataFixtures/Provider/PasswordProvider.php
@@ -4,8 +4,8 @@ namespace App\DataFixtures\Provider;
 
 class PasswordProvider
 {
-    public static function hashPassword($str)
+    public static function hashPassword($password): string
     {
-        return $str;
+        return $password;
     }
 }
