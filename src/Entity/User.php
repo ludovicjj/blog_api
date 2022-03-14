bc8543e6d73a6f88a446b52a5466c82ba3319029
ludovicjj <jahanlud@gmail.com>
2022-03-14 12:09:32 +0100

    Add plainPassword property and remove constraint notBlank to password property

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,6 +14,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -23,9 +24,11 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 #[ApiResource(
     collectionOperations: [
-        'get',
+        'get'=> [
+            'security' => 'is_granted("ROLE_USER")'
+        ],
         'post' => [
-            'security' => 'is_granted("IS_AUTHENTICATED_ANONYMOUSLY")'
+            'security' => 'is_granted("PUBLIC_ACCESS")'
         ],
         'me' => [
             'path' => '/me',
@@ -41,7 +44,9 @@ use Symfony\Component\Validator\Constraints as Assert;
         ]
     ],
     itemOperations: [
-        'get',
+        'get' => [
+            'security' => 'is_granted("ROLE_USER")'
+        ],
         'put' => [
             'security' => 'is_granted("ROLE_USER") and object == user'
         ],
@@ -49,7 +54,6 @@ use Symfony\Component\Validator\Constraints as Assert;
             'security' => 'is_granted("ROLE_ADMIN")'
         ]
     ],
-    attributes: ["security" => "is_granted('ROLE_USER')"],
     denormalizationContext: ['groups' => ['user:write'],  'swagger_definition_name' => 'write'],
     normalizationContext: ['groups' => ['user:read'], 'swagger_definition_name' => 'read']
 )]
@@ -79,10 +83,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
-     * @Assert\NotBlank()
      */
-    #[Groups(['user:write'])]
     private $password;
+
+    #[Groups(['user:write'])]
+    #[SerializedName('password')]
+    private $plainPassword;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
@@ -170,7 +176,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials()
     {
         // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
+         $this->plainPassword = null;
     }
 
     public function getUsername(): ?string
@@ -212,6 +218,16 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
             }
         }
 
+        return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+    public function setPlainPassword(string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
         return $this;
     }
 }
