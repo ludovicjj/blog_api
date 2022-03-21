206cab16c8e5da4e1b7c77713ebe9fcf35ce776f
ludovicjj <jahanlud@gmail.com>
2022-03-21 10:53:21 +0100

    log a message only when user register in API

--- src/DataPersister/UserPersister.php
+++ src/DataPersister/UserPersister.php
@@ -4,13 +4,15 @@ namespace App\DataPersister;
 
 use ApiPlatform\Core\DataPersister\DataPersisterInterface;
 use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserPersister implements DataPersisterInterface
 {
     public function __construct(
         private DataPersisterInterface $decoratedDataPersister,
-        private UserPasswordHasherInterface $hasher
+        private UserPasswordHasherInterface $hasher,
+        private LoggerInterface $appLogger
     )
     {
     }
@@ -22,6 +24,9 @@ class UserPersister implements DataPersisterInterface
 
     public function persist($data)
     {
+        if (!$data->getId()) {
+            $this->appLogger->info(sprintf('User %s just registered! Eureka!', $data->getEmail()));
+        }
         if ($data->getPlainPassword()) {
             $data->setPassword($this->hasher->hashPassword($data, $data->getPlainPassword()));
         }
