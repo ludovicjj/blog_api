397726890ac5ca4955e6c761aa3d412cb394e4d2
ludovicjj <jahanlud@gmail.com>
2022-03-22 08:29:21 +0100

    Create entity CheeseNotification

---
+++ src/Entity/CheeseNotification.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CheeseNotificationRepository;
+
+/**
+ * @ORM\Entity(repositoryClass=CheeseNotificationRepository::class)
+ */
+class CheeseNotification
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @ORM\ManyToOne(targetEntity=CheeseListing::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cheeseListing;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $notificationText;
+    public function __construct(CheeseListing $cheeseListing, string $notificationText)
+    {
+        $this->cheeseListing = $cheeseListing;
+        $this->notificationText = $notificationText;
+    }
+}
