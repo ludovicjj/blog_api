f782d4fda512bb3625dd4e761da1d1d9260420da
ludovicjj <jahanlud@gmail.com>
2022-03-23 09:21:19 +0100

    Define value to property isMe if current user is authenticated user

--- src/DataProvider/UserProvider.php
+++ src/DataProvider/UserProvider.php
@@ -6,17 +6,29 @@ use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
 
 class UserProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface
 {
-    public function __construct(private CollectionDataProviderInterface $collectionDataProvider)
+    public function __construct(
+        private CollectionDataProviderInterface $collectionDataProvider,
+        private Security $security
+    )
     {
     }
 
     public function getCollection(string $resourceClass, string $operationName = null, array $context = [])
     {
         // use the normal data provider from doctrine to handle pagination and filters
-        return $this->collectionDataProvider->getCollection($resourceClass, $operationName, $context);
+
+        /** @var User[] $users */
+        $users = $this->collectionDataProvider->getCollection($resourceClass, $operationName, $context);
+        $currentUser = $this->security->getUser();
+        foreach ($users as $user) {
+            $user->setIsMe($user === $currentUser);
+        }
+
+        return $users;
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
