ac172eeadca763a1c66634ab23cb772fb12382a1
ludovicjj <jahanlud@gmail.com>
2022-03-25 11:28:56 +0100

    Update test for DailyStatsResourceTest

--- tests/functional/DailyStatsResourceTest.php
+++ tests/functional/DailyStatsResourceTest.php
@@ -11,21 +11,12 @@ class DailyStatsResourceTest extends CustomApiTestCase
         $client = static::createClient();
         $client->request('GET', '/api/daily-stats');
         $this->assertResponseStatusCodeSame(200);
-        $this->assertJsonContains([
-            'hydra:totalItems' => 1,
-            'hydra:member' => [
-                0 => [
-                    'totalVisitors' => 100,
-                    'mostPopularListings' => []
-                ]
-            ]
-        ]);
     }
 
     public function testGetDailyStatsItem()
     {
         $client = static::createClient();
-        $client->request('GET', '/api/daily-stats/2022-02-25');
-        $this->assertResponseStatusCodeSame(404);
+        $client->request('GET', '/api/daily-stats/2020-09-03');
+        $this->assertResponseStatusCodeSame(200);
     }
 }
