7b4dbf82d684541fba32944049bd5c88e2c60208
ludovicjj <jahanlud@gmail.com>
2022-03-25 09:05:20 +0100

    Create data provider to DayliStats, provide only collection atm

--- src/DataProvider/DailyStatsProvider.php
+++ src/DataProvider/DailyStatsProvider.php
@@ -2,42 +2,24 @@
 
 namespace App\DataProvider;
 
-use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
-use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
-use ApiPlatform\Core\DataProvider\Pagination;
+use ApiPlatform\Core\DataProvider\CollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use App\Entity\DailyStats;
-use App\Filter\DailyStatsFilter;
-use App\Service\StatsHelper;
 
-class DailyStatsProvider implements ContextAwareCollectionDataProviderInterface, RestrictedDataProviderInterface,
-    ItemDataProviderInterface
+class DailyStatsProvider implements CollectionDataProviderInterface, RestrictedDataProviderInterface
 {
-
-    public function __construct(private StatsHelper $statsHelper, private Pagination $pagination)
+    public function getCollection(string $resourceClass, string $operationName = null)
     {
-    }
+        $dailyStats = (new DailyStats())
+            ->setDate(new \DateTime())
+            ->setTotalVisitors(100)
+            ->setMostPopularListings([]);
 
-    public function getCollection(string $resourceClass, string $operationName = null, array $context = []): iterable
-    {
-        list($page, $offset, $itemsPerPage) = $this->pagination->getPagination($resourceClass, $operationName, $context);
-
-        $paginator =  new DailyStatsPaginator($this->statsHelper, $page, $itemsPerPage);
-        $fromDate = $context[DailyStatsFilter::FROM_FILTER_CONTEXT] ?? null;
-        if ($fromDate) {
-            $paginator->setFromDate($fromDate);
-        }
-
-        return $paginator;
+        return [$dailyStats];
     }
 
     public function supports(string $resourceClass, string $operationName = null, array $context = []): bool
     {
         return $resourceClass === DailyStats::class;
-    }
-
-    public function getItem(string $resourceClass, $id, string $operationName = null, array $context = []): ?object
-    {
-        return $this->statsHelper->fetchOne($id);
     }
 }
