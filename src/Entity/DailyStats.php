9bd3167df311737d2daf705afca10612f2182086
ludovicjj <jahanlud@gmail.com>
2022-03-25 09:02:39 +0100

    Change property as private and add setter, getter

--- src/Entity/DailyStats.php
+++ src/Entity/DailyStats.php
@@ -15,9 +15,63 @@ use DateTimeInterface;
 )]
 class DailyStats
 {
-    public DateTimeInterface $date;
+    private DateTimeInterface $date;
 
-    public int $totalVisitors;
+    private int $totalVisitors;
 
-    public array $mostPopularListings;
+    private array $mostPopularListings;
+
+    /**
+     * @param DateTimeInterface $date
+     * @return $this
+     */
+    public function setDate(DateTimeInterface $date): self
+    {
+        $this->date = $date;
+        return $this;
+    }
+
+    /**
+     * @return DateTimeInterface
+     */
+    public function getDate(): DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param int $totalVisitors
+     * @return self
+     */
+    public function setTotalVisitors(int $totalVisitors): self
+    {
+        $this->totalVisitors = $totalVisitors;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalVisitors(): int
+    {
+        return $this->totalVisitors;
+    }
+
+    /**
+     * @param array $listing
+     * @return $this
+     */
+    public function setMostPopularListings(array $listing): self
+    {
+        $this->mostPopularListings = $listing;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMostPopularListings(): array
+    {
+        return $this->mostPopularListings;
+    }
 }
