8d74cc058ef33f63682af379b8cfb78267175c71
ludovicjj <jahanlud@gmail.com>
2022-03-28 13:23:04 +0200

    Update DailyStatsPaginator with currentPage and itemsPerPage

--- src/DataProvider/DailyStatsPaginator.php
+++ src/DataProvider/DailyStatsPaginator.php
@@ -3,42 +3,62 @@
 namespace App\DataProvider;
 
 use ApiPlatform\Core\DataProvider\PaginatorInterface;
-use Exception;
+use App\Service\StatsHelper;
 use Traversable;
 
 class DailyStatsPaginator implements PaginatorInterface, \IteratorAggregate
 {
     private $dailyStatsIterator;
 
-    public function count(): int
+    public function __construct(
+        private StatsHelper $statsHelper,
+        private int $currentPage,
+        private int $itemsPerPage
+    )
     {
-        return $this->getTotalItems();
     }
 
+    /**
+     * the number of items on this page
+     *
+     * @return int
+     * @throws \Exception
+     */
+    public function count(): int
+    {
+        return iterator_count($this->getIterator());
+    }
+
+    /**
+     * the total number of results, not just the results on this page.
+     * @return float
+     */
     public function getTotalItems(): float
     {
-        return 25;
+        return $this->statsHelper->count();
     }
 
     public function getCurrentPage(): float
     {
-        return 1;
+        return $this->currentPage;
     }
 
     public function getLastPage(): float
     {
-        return 2;
+        return ceil($this->getTotalItems() / $this->getItemsPerPage()) ?: 1.;
     }
 
     public function getItemsPerPage(): float
     {
-        return 10;
+        return $this->itemsPerPage;
     }
 
     public function getIterator(): Traversable
     {
         if ($this->dailyStatsIterator === null) {
-            $this->dailyStatsIterator = new \ArrayIterator([]);
+            $this->dailyStatsIterator = new \ArrayIterator(
+                $this->statsHelper->getMany()
+            );
         }
 
         return $this->dailyStatsIterator;
