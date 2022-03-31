1e703fbd9614991a30480bbebf1d229feba04d68
ludovicjj <jahanlud@gmail.com>
2022-03-31 11:16:42 +0200

    Init CheeseSearchFilter

---
+++ src/Filter/CheeseSearchFilter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Filter;
+
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\AbstractFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Util\QueryNameGeneratorInterface;
+use Doctrine\ORM\QueryBuilder;
+
+class CheeseSearchFilter extends AbstractFilter
+{
+
+    protected function filterProperty(string $property, $value, QueryBuilder $queryBuilder, QueryNameGeneratorInterface $queryNameGenerator, string $resourceClass, string $operationName = null)
+    {
+        // TODO: Implement filterProperty() method.
+    }
+
+    public function getDescription(string $resourceClass): array
+    {
+
+    }
+}
