08ae56695e3bbf93001802d8aee3970377842ce6
ludovicjj <jahanlud@gmail.com>
2022-04-04 17:17:11 +0200

    Implement supportsTransformation method

--- src/DataTransformer/CheeseListingInputDataTransformer.php
+++ src/DataTransformer/CheeseListingInputDataTransformer.php
@@ -3,6 +3,8 @@
 namespace App\DataTransformer;
 
 use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use App\Dto\CheeseListingInput;
+use App\Entity\CheeseListing;
 
 class CheeseListingInputDataTransformer implements DataTransformerInterface
 {
@@ -14,7 +16,10 @@ class CheeseListingInputDataTransformer implements DataTransformerInterface
 
     public function supportsTransformation($data, string $to, array $context = []): bool
     {
-        dump($data, $to, $context);
-        return false;
+        if ($data instanceof CheeseListing) {
+            return false;
+        }
+
+        return $to === CheeseListingInput::class && ($context['input']['class'] ?? null) === CheeseListingInput::class;
     }
 }
