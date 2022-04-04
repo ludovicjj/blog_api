a57540b579a215f9fe18ef6b9124692b2140e3f4
ludovicjj <jahanlud@gmail.com>
2022-04-04 19:55:06 +0200

    Implement DataProviderInitializerInterface to get current updated CheeseListing

--- src/DataTransformer/CheeseListingInputDataTransformer.php
+++ src/DataTransformer/CheeseListingInputDataTransformer.php
@@ -2,18 +2,30 @@
 
 namespace App\DataTransformer;
 
-use ApiPlatform\Core\DataTransformer\DataTransformerInterface;
+use ApiPlatform\Core\DataTransformer\DataTransformerInitializerInterface;
 use App\Dto\CheeseListingInput;
 use App\Entity\CheeseListing;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 
-class CheeseListingInputDataTransformer implements DataTransformerInterface
+class CheeseListingInputDataTransformer implements DataTransformerInitializerInterface
 {
     /**
+     * Hydrate CheeseListing with data from CheeseListingInput (DTO)
+     * If Context have no entity, create new CheeseListing (create)
+     * Else get updated Entity from context (PUT)
+     * Then change CheeseListing's properties value with CheeseListingInput data
+     *
      * @param CheeseListingInput $object
      */
     public function transform($object, string $to, array $context = []): CheeseListing
     {
-        $cheeseListing = new CheeseListing();
+        if (isset($context[AbstractNormalizer::OBJECT_TO_POPULATE])) {
+            /** @var CheeseListing $cheeseListing */
+            $cheeseListing = $context[AbstractNormalizer::OBJECT_TO_POPULATE];
+        } else {
+            $cheeseListing = new CheeseListing();
+        }
+
         $cheeseListing
             ->setTitle($object->title)
             ->setPrice($object->price)
@@ -32,5 +44,33 @@ class CheeseListingInputDataTransformer implements DataTransformerInterface
         }
 
         return $to === CheeseListing::class && ($context['input']['class'] ?? null) === CheeseListingInput::class;
+    }
+
+    /**
+     * Fetching the Entity from the Context
+     * Hydrate CheeseListingInput with value from CheeseListing.
+     * return empty or filled DTO to transform method
+     *
+     * @param string $inputClass
+     * @param array $context
+     * @return CheeseListingInput
+     */
+    public function initialize(string $inputClass, array $context = [])
+    {
+        $dto = new CheeseListingInput();
+
+        if(isset($context[AbstractNormalizer::OBJECT_TO_POPULATE])) {
+
+            /** @var CheeseListing $cheeseListing */
+            $cheeseListing = $context[AbstractNormalizer::OBJECT_TO_POPULATE];
+
+            $dto->title = $cheeseListing->getTitle();
+            $dto->description = $cheeseListing->getDescription();
+            $dto->price = $cheeseListing->getPrice();
+            $dto->isPublished = $cheeseListing->getIsPublished();
+            $dto->owner = $cheeseListing->getOwner();
+        }
+
+        return $dto;
     }
 }
