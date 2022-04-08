2a131a17b4b54ff30215877d9ca85f1895193c0b
ludovicjj <jahanlud@gmail.com>
2022-04-08 16:23:55 +0200

    Use uuid as identifier

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use App\Controller\MeController;
@@ -97,11 +98,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
+    #[ApiProperty(identifier: false)]
     private $id;
 
     /**
      * @ORM\Column(type="uuid", unique=true)
      */
+    #[ApiProperty(identifier: true)]
     private $uuid;
 
     /**
